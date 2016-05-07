d473e02cdf2fd93dedd9946d7b001814d3c9a386
stan-chuk <Stan@Mac-mini.local>
2016-05-07 22:54:11 +0800

    initial commit

---
+++ FMPlayer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FMPlayer
+//
+//  Created by 祝韶明 on 16/5/7.
+//  Copyright © 2016年 祝韶明. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
